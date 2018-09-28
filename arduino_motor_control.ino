19891b77934f145b826b7c01cfeb3d12182d2cae
Jiwon Park <qwes8873@gmail.com>
2018-09-28 20:04:01 +0900

    [Refactor] Seperate functions and modify some types of variables

--- arduino_motor_control.ino
+++ arduino_motor_control.ino
@@ -1,37 +1,37 @@
-#define FOSC 16000000
-#define BAUD 1000000
+#define FOSC 16000000 // Frequency Of System Clock
+#define BAUD 1000000  // Baudrate
 
 #define NUMBER_DXL 2
-
-unsigned int MYUBRR = 0;
 
 // flag
 bool send_data_to_matlab = false;
 bool interrupt_on = true;
-bool infinite_mode = false;
+bool infinite_mode = false;       // true: 무한궤적모드, false: 단일궤적모드
 bool start_flag = true;
 
 // counter
-unsigned long ISRcnt = 0;    // WARNING: ISRcnt를 int로 하면 매트랩 정지 현상 생김
-int motorCnt[NUMBER_DXL] = {0};
-int motorCntEnd[NUMBER_DXL];
+unsigned long ISRcnt = 0;         // WARNING: ISRcnt를 int로 하면 매트랩 정지 현상 생김
+unsigned int motorCnt[NUMBER_DXL] = {0};   // Motor의 goal을 정할 때 사용하는 count
+unsigned int motorCntEnd[NUMBER_DXL];      // MotorCnt의 목표값
 
 // degree
-int pastDegree[NUMBER_DXL];
-unsigned int goalDegree[NUMBER_DXL];  // 지금 움직여야 하는 모터 각도
-unsigned int curDegreeBuf[NUMBER_DXL] = {0};
+unsigned int pastDegree[NUMBER_DXL];                   // 전 모터 위치(각도)
+unsigned int goalDegree[NUMBER_DXL];          // 지금 움직여야 하는 모터 위치
+unsigned int curDegreeBuf[NUMBER_DXL] = {0};  // 현재 모터 위치
 
 // packet buffer
-unsigned char readpacketBuf[20];
+unsigned char returnPacketBuf[20];
 
 // Values from matlab
-unsigned int motorID[NUMBER_DXL] = {1, 2};
-int inputDegree[NUMBER_DXL];
-int inputFreq[NUMBER_DXL];
+unsigned int motorID[NUMBER_DXL] = {1, 2}; 
+unsigned int inputDegree[NUMBER_DXL];
+unsigned int inputFreq[NUMBER_DXL];
 
 // pin number
-int ledPin = 13;
-int controlPin = 4;
+char ledPin = 13;
+char controlPin = 4;   // 74ls241 컨트롤 핀
+
+unsigned int MYUBRR = 0;
 
 void setup() {
   pinMode(controlPin, OUTPUT);
@@ -51,7 +51,7 @@ void setup() {
   TIMSK1 |= (1 << OCIE1A);
 
   // Arduino Mega의 TX1, RX1 사용하기 위함 (모터와 통신)
-  UCSR1A |= (1 << U2X1); //U2X0-->통신속도를 2배로 해주는 세팅용 비트
+  UCSR1A |= (1 << U2X1); //통신속도를 2배로 해주는 세팅용 비트
   UCSR1B |= (1 << RXEN1) | (1 << TXEN1);
   UCSR1C |= (1 << UCSZ11) | (1 << UCSZ01);
   MYUBRR = (FOSC / (8 * BAUD)) - 1;
@@ -69,6 +69,7 @@ void setup() {
 /* 모터 다음값 결정 */
 double calMotorValue(double goal, double fre, int cnt, double current)
 {
+  // 1-cos 함수 적용
   return double(current + ((goal-current)/2.0)*(1-cos((2*3.14/(fre*10.0))*cnt)));
 }
 
@@ -79,7 +80,7 @@ void USART_Transmit_for_1(unsigned char SEND)
 }
 
 /* 모터에 Syncwrite 패킷을 생성하여 보냄 */
-void writeMotor()
+void syncWrite()
 {
   unsigned char writepacket[20];
 
@@ -157,132 +158,157 @@ void blinkLed()
   delay(1000); 
 }
 
-/* 매트랩으로부터 값을 받아옴 */
-void data_reading_from_matlab()
+/* 매트랩으로부터 온 패킷을 읽어 모드를 정함 */
+void setMode()
 {
-  int i;
-  int MID[20];
+  for(int i = 0; i < 1;)
+  {
+    if(Serial.available() > 0)
+    {
+      char packet = Serial.read();
+      char mode = packet - '0';
+      
+      if(mode == 0)
+      {
+        infinite_mode = false;
+      }
+      else if(mode == 1)
+      {
+        infinite_mode = true;
+      }
+      else if(mode == 2)  // stop
+      {
+        for(int i = 0; i < NUMBER_DXL; i++)
+        {
+          pastDegree[i] = goalDegree[i];
+          motorCnt[i] = motorCntEnd[i];
+          infinite_mode = false;
+        }
+      }
+      i++;
+    }
+  }
+}
 
+/* 매트랩으로부터 온 패킷을 읽어 움직일 모터의 ID를 정함 */
+void getMotorID(char ID)
+{
+  char MID[2];
+  
+  for(int i = 0; i < 2; )
+  {
+    if(Serial.available() > 0)
+    {
+      char packet = Serial.read();
+      char ID2 = packet - '0';
+      MID[i] = ID2;
+      i++;              // WARNING: 이 구문을 for문 선언안에 넣게 되면 패킷을 받아 이곳이 호출되기 전에 빠져나간다.
+    }
+  }
+
+  int idx;
+  if(ID == 49)
+  {
+    idx = 0;
+  }
+  else if(ID == 52)
+  {
+    idx = 1;
+  }
+  motorID[idx] = (MID[0]*10) + (MID[1]*1);
+}
+
+/* 매트랩으로부터 온 패킷을 읽어 모터의 목표 위치를 정함 */
+void getMotorDegree(char ID)
+{
+  char MID[4];
+  
+  for(int i = 0; i < 4;)
+  {
+    if(Serial.available() > 0)
+    {
+      char ID1 = Serial.read();
+      char ID2 = ID1 - '0';
+      MID[i] = ID2;
+      i++;
+    }
+  }
+
+  char idx;
+  if(ID == 50)
+  {
+    idx = 0;
+  }
+  else if(ID == 53)
+  {
+    idx = 1;
+  }
+
+  inputDegree[idx] = (MID[0]*1000) + (MID[1]*100) + (MID[2]*10) + (MID[3]*1);
+  motorCnt[idx] = 0;
+}
+
+/* 매트랩으로부터 온 패킷을 읽어 제어 주기를 정함 */
+void getMotorFreq(char ID)
+{
+  char MID[4];
+  
+  for(int i = 0; i < 4; )
+  {
+    if(Serial.available() > 0)
+    {
+      char ID1 = Serial.read();
+      char ID2 = ID1 - '0';
+      MID[i] = ID2;
+      i++;
+    }
+  }
+  
+  char idx;
+  if(ID == 51)
+  {
+    idx = 0;
+  }
+  else if(ID == 54)
+  {
+    idx = 1;
+  }
+  
+  inputFreq[idx] = (MID[0]*1000) + (MID[1]*100) + (MID[2]*10) + (MID[3]*1);
+  motorCntEnd[idx] = 5 * inputFreq[idx];   // 1 - cos의 반주기
+}
+  
+/* 매트랩으로부터 값을 받아옴 */
+void readDataFromMatlab()
+{  
   if(Serial.available() > 0)
   {
     char ID1 = Serial.read();
-    int ID = ID1 - '0';
+    char ID = ID1 - '0';
 
-    if(ID == 73)
+    if(ID == 73)  // ID1 = 'y'
     {
       interrupt_on = false;
     }
-    else if(ID == 74)
+    else if(ID == 74)  // ID1 = 'z' 
     {
       interrupt_on = true;
     }
-    else if(ID == 61)   // 'm' -> mode (0: finite, 1: infinite mode, 2: stop mode)
+    else if(ID == 61)   // ID1 = 'm' -> mode (0: finite, 1: infinite mode, 2: stop mode)
     {
-      for(int i = 0; i < 1;)
-      {
-        if(Serial.available() > 0)
-        {
-          char ID1 = Serial.read();
-          int ID = ID1 - '0';
-          
-          if(ID == 0)
-          {
-            infinite_mode = false;
-          }
-          else if(ID == 1)
-          {
-            infinite_mode = true;
-          }
-          else if(ID == 2)
-          {
-            for(int i = 0; i < NUMBER_DXL; i++)
-            {
-              pastDegree[i] = goalDegree[i];
-              motorCnt[i] = motorCntEnd[i];
-              infinite_mode = false;
-            }
-          }
-          i++;
-        }
-      }
+      setMode();
     }
-    else if(ID == 49 || ID == 52)
+    else if(ID == 49 || ID == 52)   // ID1 = 'a' or 'd'
     {
-      for(i = 0; i < 2; )
-      {
-        if(Serial.available() > 0)
-        {
-          char ID1 = Serial.read();
-          int ID = ID1 - '0';
-          MID[i] = ID;
-          i++;              // WARNING: 이 구문을 for문 선언안에 넣게 되면 패킷을 받아 이곳이 호출되기 전에 빠져나간다.
-        }
-      }
-
-      int idx;
-      if(ID == 49)
-      {
-        idx = 0;
-      }
-      else if(ID == 52)
-      {
-        idx = 1;
-      }
-      motorID[idx] = (MID[0]*10) + (MID[1]*1);
-
+      getMotorID(ID);
     }
-    else if(ID == 50 || ID == 53)
+    else if(ID == 50 || ID == 53)   // ID1 = 'b' or 'e'
     {
-      for(i = 0; i < 4;)
-      {
-        if(Serial.available() > 0)
-        {
-          char ID1 = Serial.read();
-          int ID = ID1 - '0';
-          MID[i] = ID;
-          i++;
-        }
-      }
-
-      int idx;
-      if(ID == 50)
-      {
-        idx = 0;
-      }
-      else if(ID == 53)
-      {
-        idx = 1;
-      }
-
-      inputDegree[idx] = (MID[0]*1000) + (MID[1]*100) + (MID[2]*10) + (MID[3]*1);
-      motorCnt[idx] = 0;    // FIXME: 'z' 읽은 다음에 초기화?? (최대한 늦게?)
-      
+      getMotorDegree(ID);
     }
-    else if(ID == 51 || ID == 54)
+    else if(ID == 51 || ID == 54)   // ID1 = 'c' or 'f'
     {
-      for(i = 0; i < 4; )
-      {
-        if(Serial.available() > 0)
-        {
-          char ID1 = Serial.read();
-          int ID = ID1 - '0';
-          MID[i] = ID;
-          i++;
-        }
-      }
-      
-      int idx;
-      if(ID == 51)
-      {
-        idx = 0;
-      }
-      else if(ID == 54)
-      {
-        idx = 1;
-      }
-      
-      inputFreq[idx] = (MID[0]*1000) + (MID[1]*100) + (MID[2]*10) + (MID[3]*1);
-      motorCntEnd[idx] = 5 * inputFreq[idx];   // 1 - cos의 반주기
+      getMotorFreq(ID); 
     }
     /*
     if(motorID[0] == 1 && inputDegree[0] == 1000 && inputFreq[0] == 1000
@@ -294,76 +320,85 @@ void data_reading_from_matlab()
   }
 }
 
+/* 리턴 패킷을 읽음 */
+void readReturnPacket()
+{
+  for(int i = 0; i < 7;)
+  {
+    if(Serial1.available() > 0)
+    {
+      returnPacketBuf[i] = Serial1.read();
+      
+      if(i == 1)
+      {
+        if(!(returnPacketBuf[1] == 1 || returnPacketBuf[1] == 2))
+        {
+          break;
+        }
+      }
+      //Serial.println(returnPacketBuf[i]);
+      i++;
+    }
+  }
+  //Serial.println("-----");
+}
+
+/* 리턴패킷을 파싱하여 현재 모터 위치를 저장 */
+void storeCurDegree()
+{
+  unsigned char sumOfPacket = 0;
+          
+  for(int i = 1; i < 6; i++)
+  {
+    sumOfPacket += returnPacketBuf[i];
+  }
+  sumOfPacket = ~byte(sumOfPacket);   // 다시 넣어주어야 ~byte연산한 비트들을 unsigned char로 해석하게 되고 비교시 문제가 없음
+
+  int tmp = returnPacketBuf[5] << 8;
+  tmp = tmp + returnPacketBuf[4];
+  if(sumOfPacket == returnPacketBuf[6] && tmp > 0 && tmp < 4096)
+  {
+    // ID == 1
+    if(returnPacketBuf[1] == 1)
+    {
+      curDegreeBuf[0] = tmp;
+      // 처음에 pastDegree, goalDegree 초기화
+      if(start_flag == true)
+      {
+        pastDegree[0] = tmp;
+        goalDegree[0] = (double)tmp;
+      }
+    }
+    // ID == 2
+    else if(returnPacketBuf[1] == 2)
+    {
+      curDegreeBuf[1] = tmp;
+      // 처음에 pastDegree, goalDegree 초기화
+      if(start_flag == true)
+      {
+        pastDegree[1] = tmp;
+        goalDegree[1] = (double)tmp;
+        start_flag = false;
+      }
+    }
+  }
+}
+
 /* 모터로부터 리턴패킷을 받아옴 */
-void data_reading_from_motor_buf()
+void readDataFromMotorBuf()
 {
   if(Serial1.available() > 0)
   {
-    
     if(Serial1.read() == 0xFF && Serial1.peek() == 0xFF)  // 두번째 peek으로 안하고 read로 하면 문제 생김
     {
-      
-      for(int i = 0; i < 7;)
-      {
-        if(Serial1.available() > 0)
-        {
-          readpacketBuf[i] = Serial1.read();
-          
-          if(i == 1)
-          {
-            if(!(readpacketBuf[1] == 1 || readpacketBuf[1] == 2))
-            {
-              break;
-            }
-          }
-          //Serial.println(readpacketBuf[i]);
-          i++;
-        }
-      }
-      //Serial.println("-----");
-      unsigned char sumOfPacket = 0;
-
-          
-      for(int i = 1; i < 6; i++)
-      {
-        sumOfPacket += readpacketBuf[i];
-      }
-      sumOfPacket = ~byte(sumOfPacket);   // 다시 넣어주어야 ~byte연산한 비트들을 unsigned char로 해석하게 되고 비교시 문제가 없음
-
-      int tmp = readpacketBuf[5] << 8;
-      tmp = tmp + readpacketBuf[4];
-      if(sumOfPacket == readpacketBuf[6] && tmp > 0 && tmp < 4096)
-      {
-        // ID == 1
-        if(readpacketBuf[1] == 1)
-        {
-          curDegreeBuf[0] = tmp;
-          // 처음에 pastDegree, goalDegree 초기화
-          if(start_flag == true)
-          {
-            pastDegree[0] = tmp;
-            goalDegree[0] = (double)tmp;
-          }
-        }
-        // ID == 2
-        else if(readpacketBuf[1] == 2)
-        {
-          curDegreeBuf[1] = tmp;
-          // 처음에 pastDegree, goalDegree 초기화
-          if(start_flag == true)
-          {
-            pastDegree[1] = tmp;
-            goalDegree[1] = (double)tmp;
-            start_flag = false;
-          }
-        }
-      }
+      readReturnPacket();
+      storeCurDegree();
     } 
   }
 }
 
 /* 매트랩에 모터의 현재 위치 값 보내기 */
-void data_sending_to_matlab()
+void sendDataToMatlab()
 {
   for(int i = 0; i < NUMBER_DXL; i++)
   {
@@ -372,73 +407,76 @@ void data_sending_to_matlab()
   send_data_to_matlab = false;
 }
 
+/* 모터 순간 목표 위치 계산 */
+void calMotorGoal(char motorId)
+{
+  motorCnt[motorId]+=5;   // 5씩 증가를 시켜야 함 (이 if문에 5초에 한번씩 들어오기 때문)
+  
+  double tmp_goal = calMotorValue(inputDegree[motorId], inputFreq[motorId], 
+                                    motorCnt[motorId], pastDegree[motorId]);
+  goalDegree[motorId] = tmp_goal;
+}
+
+/* 모터 움직임 */
+void moveMotors()
+{
+  digitalWrite(controlPin, HIGH);   // 74ls241칩을 tx로 바꿈
+  for(int i = 0; i < NUMBER_DXL; i++)
+  {
+    if(infinite_mode == true)   // 무한궤적모드
+    {
+      calMotorGoal(i);
+    }
+    else   // 단일궤적모드
+    {
+      if(motorCnt[i] < motorCntEnd[i])   // ISR -> 5ms마다 호출 => goal을 반주기로 나눠서 여러번 보냄?
+      {
+        calMotorGoal(i);
+      } 
+      else
+      {
+        if(start_flag == false)
+        { 
+          pastDegree[i] = goalDegree[i];  // 목표에 도달 후 pastDegree값 현재값(현재 goal)로 초기화
+        }
+        Serial.flush();
+      }
+    } // infinite_mode else end
+  } // for loop end
+  syncWrite();
+}
+
 /* 모터 컨트롤을 위한 인터럽트 */
 ISR(TIMER1_COMPA_vect)
 {
   ISRcnt++;
   if(interrupt_on == true)
   {
-    
-  if(ISRcnt % 5 == 1)
-  {
-    digitalWrite(controlPin, HIGH);
- 
-    for(int i = 0; i < NUMBER_DXL; i++)
+    if(ISRcnt % 5 == 1)   // 5ms마다 sync write 수행
     {
-      if(infinite_mode == true)
-      {
-        motorCnt[i]+=5;
-        
-        double tmp_goal = calMotorValue(inputDegree[i], inputFreq[i], motorCnt[i], pastDegree[i]);
-        goalDegree[i] = tmp_goal;
-      
-      }
-      else
-      {
-       
-      if(motorCnt[i] < motorCntEnd[i])   // ISR -> 5ms마다 호출 => goal을 반주기로 나눠서 여러번 보냄?
-      {
-        motorCnt[i]+=5;    // 5씩 증가를 시켜야 함 (이 if문에 5초에 한번씩 들어오기 때문)
-        
-        double tmp_goal = calMotorValue(inputDegree[i], inputFreq[i], motorCnt[i], pastDegree[i]);
-        goalDegree[i] = tmp_goal;
-      } 
-      else
-      {
-        // 다 움직인 후 pastDegree값 현재값(현재 goal)로 초기화
-        if(start_flag == false)
-        { 
-          pastDegree[i] = goalDegree[i];
-        }
-        Serial.flush();
-      }
-      
-      } // infinite_mode else end
+      moveMotors();
     }
-    writeMotor();
+    else if(ISRcnt % 5 == 3)
+    {
+      bulkRead();
+    }
   }
-  else if(ISRcnt % 5 == 3)
-  {
-    bulkRead();
-  }
-  }
-  
+
   // 50ms마다 한번 씩 매트랩으로 값을 쏴준다
   if(ISRcnt % 50 == 10)   // bulkRead하는 시점과 조금 텀이 있어야!
   {
     send_data_to_matlab = true;
-  }
- 
+  } 
 }
 
 void loop() 
 {
-  data_reading_from_matlab();
+  readDataFromMatlab();
 
-  data_reading_from_motor_buf();
+  readDataFromMotorBuf();
 
   if(send_data_to_matlab == true)
   {
-    data_sending_to_matlab();
+    sendDataToMatlab();
   }
 }
